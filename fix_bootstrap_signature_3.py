035123bbd05cb4b030b4f768920f2b717f9b9a9b
Peter Schultz <pmesch@gmail.com>
2026-03-21 08:54:44 -0600

    cleanup
    python patch scripts

--- fix_bootstrap_signature_3.py
+++
@@ -1,5 +0,0 @@
-with open('libs/quartzite/src/platforms/gtk/spline.rs', 'r') as f:
-    code = f.read()
-
-# Let's inspect the actual signature in the current file first
-print(repr(code[code.find('pub fn bootstrap('):code.find('pub fn bootstrap(')+250]))
