a4c76bac2af1427d723f44f2db86114337436a0b
Peter Schultz <pmesch@gmail.com>
2026-03-23 07:59:12 -0600

    remove
    patch files

--- patch_nexus.py
+++
@@ -1,7 +0,0 @@
-with open('libs/quartzite/src/platforms/qt/assets/qml/NexusPanel.qml', 'r') as f:
-    content = f.read()
-
-content = content.replace('text: display', 'text: matrixLabel')
-
-with open('libs/quartzite/src/platforms/qt/assets/qml/NexusPanel.qml', 'w') as f:
-    f.write(content)
